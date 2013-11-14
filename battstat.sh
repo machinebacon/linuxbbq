decec276e65e6850bb96b51d8046a9e534ed4c2d
machinebacon <machinebacon@users.noreply.github.com>
2013-11-14 19:39:56 +0800

    Update battstat.sh

--- battstat.sh
+++ battstat.sh
@@ -1,2 +1,6 @@
 #! /bin/bash
-cat /sys/class/power_supply/BAT0/capacity
+if test -e /sys/class/power_supply/BAT0/capacity; then
+  cat /sys/class/power_supply/BAT0/capacitycat /sys/class/power_supply/BAT0/capacity >&2
+else
+  exit 1
+fi
